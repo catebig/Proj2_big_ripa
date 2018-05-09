Fix some forwards and start the backprop for the linear module

Modules/Module_base.py
@@ -10,7 +10,6 @@ class Module(object):
   def forward(self, *input):
     raise NotImplementedError
 
-  @abstractmethod
   def backward(self, *gradwrtoutput):
     raise NotImplementedError
 
@@ -33,14 +32,22 @@ class Linear(Module):
 
   def __init__(self, *dim):
     self.weights = torch.randn(*dim)
-    self.bias = torch.randn(*dim[:-1],1)
+    self.bias = torch.randn(1,*dim[:-1])
+    self.gradwrtweights = torch.zeros(*dim)
+    self.gradwrtbias = torch.zeros(1,*dim[:-1])
 
   def forward(self, *input):
     self.input = input[0]
-    return torch.mm(self.weights,input[0])+self.bias
+    return torch.mm(self.input,self.weights.t())+self.bias
 
-  def backward(self, *gradwrtoutput):
-    raise NotImplementedError
+  def backward(self, gradwrtoutput):
+    # Compute gradient with respect to input
+    gradwrtinput = gradwrtoutput * self.weights
+    # Compute derivatives of loss wrt parameters
+    self.gradwrtweights = 
+    self.gradwrtbias = torch.mean(gradwrtinput,0)
+
+    return gradwrtinput
 
   def param(self):
       return []
@@ -51,10 +58,11 @@ class LossMSE(Module):
 
   def forward(self, *input):
     self.diff = input[0] - input[1]
-    return torch.mm(torch.t(self.diff),self.diff)/torch.numel(self.diff)
+    loss = torch.sum(torch.pow(self.diff,2),1)/self.diff.size(0)
+    return loss 
 
   def backward(self, *gradwrtoutput):
-    return 2*self.diff/torch.numel(self.diff)
+    return 2*self.diff/self.diff.size(0)
 
   def param(self):
     return []
@@ -65,15 +73,16 @@ class LossMSE(Module):
 
 class Sequential(Module):
 
-  def __init__(self,listModules):
+  def __init__(self,listModules,loss):
     self.graphModules = listModules
+    self.loss = loss
 
   def forward(self, *input):
     x = input[0]
     for Module in self.graphModules:
       x = Module.forward(x)
     return x
-
+  
   def backward(self):
      raise NotImplementedError
   

driver.py
@@ -8,15 +8,18 @@ import torch
 
 # Create List of Modules (first attempt of NN structure)
 listModules = []
-listModules.append(M.Linear(5,5))
+number_of_outputs = 2
+number_of_inputs = 2
+listModules.append(M.Linear(number_of_outputs,number_of_inputs))
 listModules.append(M.Sigmoid())
 # Give the list as input to a sequential Module
-NN = M.Sequential(listModules)
+loss = M.LossMSE()
+NN = M.Sequential(listModules,loss)
 
 loss = M.LossMSE()
-y = torch.randn(5,1)
-x = torch.randn(5,1)
+y = torch.randn(10,number_of_outputs)
+x = torch.randn(10,number_of_inputs)
 c = NN.forward(x)
 r = loss.forward(c,y)
 print(c)
-print(r)
+#print(r)